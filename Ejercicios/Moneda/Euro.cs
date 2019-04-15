edfd81ee59a8ae126b5afe63fb783caa9122f66d
cristian schlieter <c.schlieter@hotmail.com>
2019-04-15 17:26:54 -0300

    commit

--- Ejercicios/Moneda/Euro.cs
+++ Ejercicios/Moneda/Euro.cs
@@ -6,41 +6,67 @@ using System.Threading.Tasks;
 
 namespace Moneda
 {
-  public class Euro
-  {
+    public class Euro
+    {
 
-    private double cantidad;
-    private static float cotizRespectoDolar;
+        private double cantidad;
+        private static float cotizRespectoDolar;
 
-    static Euro()
-    {
-      Euro.cotizRespectoDolar = 0.89f;
-    }
-    public Euro(double cantidad)
-    {
-      this.cantidad = cantidad;
-    }
-    public Euro(double cantidad, float cotizacion) : this(cantidad)
-    {
-      Euro.cotizRespectoDolar = cotizacion;
-    }
+        static Euro()
+        {
+            Euro.cotizRespectoDolar = 0.89f;
+        }
+        public Euro(double cantidad)
+        {
+            this.cantidad = cantidad;
+        }
+        public Euro(double cantidad, float cotizacion) : this(cantidad)
+        {
+            Euro.cotizRespectoDolar = cotizacion;
+        }
 
-    public double GetCantidad() { return this.cantidad; }
-    public static float GetCotizacion() { return Euro.cotizRespectoDolar; }
+        public double GetCantidad() { return this.cantidad; }
+        public static float GetCotizacion() { return Euro.cotizRespectoDolar; }
 
-    public static explicit operator Dolar(Euro e)
-    {
-      return new Dolar(e.GetCantidad() * Euro.GetCotizacion());
+        public static explicit operator Dolar(Euro e)
+        {
+            return new Dolar(e.GetCantidad() * Euro.GetCotizacion());
+        }
+        public static explicit operator Pesos(Euro e)
+        {
+            Dolar aux = (Dolar)e;
+            //var aux = (Dolar)e;
+            return new Pesos(aux.GetCantidad() * Pesos.GetCotizacion());
+        }
+        public static implicit operator Euro(double d)
+        {
+            return new Euro(d);
+        }
+        
+        public static bool operator ==(Euro e, Dolar d)
+        {
+            return (((Dolar)e).GetCantidad() == d.GetCantidad());
+        }
+        public static bool operator !=(Euro e, Dolar d)
+        {
+            return !(e == d);
+        }
+
+        public static bool operator ==(Euro e, Pesos p)
+        {
+            return (((Dolar)e).GetCantidad() == ((Dolar)p).GetCantidad());
+        }
+        public static bool operator !=(Euro e, Pesos p)
+        {
+            return !(e == p);
+        }
+        public static bool operator ==(Euro e1, Euro e2)
+        {
+            return e1.GetCantidad() == e2.GetCantidad();
+        }
+        public static bool operator !=(Euro e1, Euro e2)
+        {
+            return !(e1 == e2);
+        }
     }
-    public static explicit operator Pesos(Euro e)
-    {
-      Dolar aux = (Dolar)e;
-      //var aux = (Dolar)e;
-      return new Pesos(aux.GetCantidad() * Pesos.GetCotizacion());
-    }
-    public static implicit operator Euro(double d)
-    {
-      return new Euro(d);
-    }
-  }
 }
--- Ejercicios/Moneda/Pesos.cs
+++ Ejercicios/Moneda/Pesos.cs
@@ -6,53 +6,94 @@ using System.Threading.Tasks;
 
 namespace Moneda
 {
-  public class Pesos
-  {
-    private double cantidad;
-    private static float cotizRespectoDolar;
-
-    static Pesos()
+    public class Pesos
     {
-      Pesos.cotizRespectoDolar = 0.023f;
-    }
-    public Pesos(double cantidad)
-    {
-      this.cantidad = cantidad;
-    }
-    public Pesos(double cantidad, float cotizacion):this(cantidad)
-    {
-      Pesos.cotizRespectoDolar = cotizacion;
-    }
+        private double cantidad;
+        private static float cotizRespectoDolar;
 
-    public double GetCantidad() { return this.cantidad; }
-    public static float GetCotizacion() {  return Pesos.cotizRespectoDolar; }
+        static Pesos()
+        {
+            Pesos.cotizRespectoDolar = 0.023f;
+        }
+        public Pesos(double cantidad)
+        {
+            this.cantidad = cantidad;
+        }
+        public Pesos(double cantidad, float cotizacion) : this(cantidad)
+        {
+            Pesos.cotizRespectoDolar = cotizacion;
+        }
 
-    public static explicit operator Dolar(Pesos p)
-    {
-      return new Dolar(p.GetCantidad() * Pesos.GetCotizacion());
+        public double GetCantidad() { return this.cantidad; }
+        public static float GetCotizacion() { return Pesos.cotizRespectoDolar; }
+
+        public static explicit operator Dolar(Pesos p)
+        {
+            return new Dolar(p.GetCantidad() * Pesos.GetCotizacion());
+        }
+
+        public static explicit operator Euro(Pesos p)
+        {
+            var aux = (Dolar)p;
+
+            return new Euro(aux.GetCantidad() * Euro.GetCotizacion());
+        }
+        public static implicit operator Pesos(double d)
+        {
+            return new Pesos(d);
+        }
+
+        public static bool operator ==(Pesos p, Dolar d)
+        {
+            return (((Dolar)p).GetCantidad() == d.GetCantidad());
+        }
+
+        public static bool operator !=(Pesos p, Dolar d)
+        {
+            return !(p == d);
+        }
+        public static bool operator ==(Pesos p1, Pesos p2)
+        {
+            return (p1.GetCantidad() == p2.GetCantidad());
+        }
+        public static bool operator !=(Pesos p1, Pesos p2)
+        {
+            return !(p1 == p2);
+        }
+
+        public static bool operator ==(Pesos p, Euro e)
+        {
+            return (((Dolar)e).GetCantidad() == ((Dolar)p).GetCantidad());
+        }
+        public static bool operator !=(Pesos p, Euro e)
+        {
+            return !(p == e);
+        }
+        public static Pesos operator +(Pesos p, Dolar d)
+        {
+            double resultado;
+            resultado = ((Dolar)p).GetCantidad() + d.GetCantidad();
+            return ((Pesos)resultado).GetCantidad();
+        }
+        public static Pesos operator -(Pesos p, Dolar d)
+        {
+            double resultado;
+            resultado = ((Dolar)p).GetCantidad() - d.GetCantidad();
+            return ((Pesos)resultado).GetCantidad();
+        }
+        public static Pesos operator +(Pesos p, Euro e)
+        {
+            double resultado;
+            resultado = ((Dolar)p).GetCantidad() + ((Dolar)e).GetCantidad();
+            return ((Pesos)resultado).GetCantidad();
+        }
+        public static Pesos operator -(Pesos p, Euro e)
+        {
+            double resultado;
+            resultado = ((Dolar)p).GetCantidad() - ((Dolar)e).GetCantidad();
+            return ((Pesos)resultado).GetCantidad();
+        }
+
+
     }
-
-    public static explicit operator Euro(Pesos p)
-    {
-      var aux = (Dolar)p;
-     
-      return new Euro(aux.GetCantidad() * Euro.GetCotizacion());
-    }
-    public static implicit operator Pesos(double d)
-    {
-      return new Pesos(d);
-    }
-
-    public static bool operator ==(Pesos p, Dolar d)
-    {
-      return (((Dolar)p).GetCantidad() == d.GetCantidad());
-    }
-
-    public static bool operator !=(Pesos p, Dolar d)
-    {
-      return !(p == d);
-    }
-
-
-  }
 }
