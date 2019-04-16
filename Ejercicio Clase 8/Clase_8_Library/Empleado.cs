473ac1bb6ab9201f6347f54d3925b393a1d24559
cristian schlieter <c.schlieter@hotmail.com>
2019-04-16 17:19:53 -0300

    commit

--- Ejercicio Clase 8/Clase_8_Library/Empleado.cs
+++ Ejercicio Clase 8/Clase_8_Library/Empleado.cs
@@ -6,61 +6,68 @@ using System.Threading.Tasks;
 
 namespace Clase_8_Library
 {
-  public class Empleado
-  {
-    public enum EPuestoJerarquico
+    public class Empleado
     {
-      Administración,
-      Gerencia,
-      Sistemas,
-      Accionista
+        public enum EPuestoJerarquico
+        {
+            Administración,
+            Gerencia,
+            Sistemas,
+            Accionista
+        }
+        string _nombre;
+        string _apellido;
+        string _legajo;
+        EPuestoJerarquico _puesto;
+        int _salario;
+
+        public Empleado(string nombre, string apellido, string legajo, EPuestoJerarquico puesto, int salario)
+        {
+            this._nombre = nombre;
+            this._apellido = apellido;
+            this._legajo = legajo;
+            this._salario = salario;
+            this._puesto = puesto;
+        }
+
+        /// <summary>
+        /// Dos empleados son iguales si, y sólo si, comparten el mismo número de legajo.
+        /// </summary>
+        /// <param name="e1">Primer empleado a comparar</param>
+        /// <param name="e2">Segundo empleado a comparar</param>
+        /// <returns></returns>
+        public static bool operator ==(Empleado e1, Empleado e2)
+        {
+            if (e1 is null || e2 is null || e1._legajo != e2._legajo) { return false; }
+            else { return true; }
+            // Controlo que ninguno de los dos empleados no haya sido instanciado, para evitar errores.
+            //alumno
+        }
+
+        /// <summary>
+        /// Dos empleados son distintos si, y sólo si, NO comparten el mismo número de legajo.
+        /// </summary>
+        /// <param name="e1">Primer empleado a comparar</param>
+        /// <param name="e2">Segundo empleado a comparar</param>
+        /// <returns></returns>
+        public static bool operator !=(Empleado e1, Empleado e2)
+        {
+            return !(e1 == e2);
+            //alumno
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Nombre  : " + this._nombre);
+            sb.AppendLine("Apellido: " + this._apellido);
+            sb.AppendLine("Legajo  : " + this._legajo);
+            sb.AppendLine("Puesto  : " + this._puesto.ToString());
+            sb.AppendLine("Salario : $" + this._salario);
+            sb.AppendLine("******************");
+
+            return sb.ToString();
+        }
     }
-    string _nombre;
-    string _apellido;
-    string _legajo;
-    EPuestoJerarquico _puesto;
-    int _salario;
-
-    public Empleado(string nombre, string apellido, string legajo, EPuestoJerarquico puesto, int salario)
-    {
-      //alumno
-    }
-
-    /// <summary>
-    /// Dos empleados son iguales si, y sólo si, comparten el mismo número de legajo.
-    /// </summary>
-    /// <param name="e1">Primer empleado a comparar</param>
-    /// <param name="e2">Segundo empleado a comparar</param>
-    /// <returns></returns>
-    public static bool operator ==(Empleado e1, Empleado e2)
-    {
-      // Controlo que ninguno de los dos empleados no haya sido instanciado, para evitar errores.
-      //alumno
-    }
-
-    /// <summary>
-    /// Dos empleados son distintos si, y sólo si, NO comparten el mismo número de legajo.
-    /// </summary>
-    /// <param name="e1">Primer empleado a comparar</param>
-    /// <param name="e2">Segundo empleado a comparar</param>
-    /// <returns></returns>
-    public static bool operator !=(Empleado e1, Empleado e2)
-    {
-      //alumno
-    }
-
-    public string Mostrar()
-    {
-      StringBuilder sb = new StringBuilder();
-
-      sb.AppendLine("Nombre  : " + this._nombre);
-      sb.AppendLine("Apellido: " + this._apellido);
-      sb.AppendLine("Legajo  : " + this._legajo);
-      sb.AppendLine("Puesto  : " + this._puesto.ToString());
-      sb.AppendLine("Salario : $" + this._salario);
-      sb.AppendLine("******************");
-
-      return sb.ToString();
-    }
-  }
 }
--- Ejercicio Clase 8/Clase_8_Library/Empresa.cs
+++ Ejercicio Clase 8/Clase_8_Library/Empresa.cs
@@ -6,88 +6,97 @@ using System.Threading.Tasks;
 
 namespace Clase_8_Library
 {
-  public class Empresa
-  {
-    List<Empleado> _nominaEmpleados;
-    string _razonSocial;
-    string _direccion;
-    float _ganancias;
+    public class Empresa
+    {
+        List<Empleado> _nominaEmpleados;
+        string _razonSocial;
+        string _direccion;
+        float _ganancias;
 
-    #region "Constructores"
-    private Empresa()
-    {
-      //alumno
-    }
-    public Empresa(string razonSocial, string direccion, float ganancias)
-    {
-      this._razonSocial = razonSocial;
-      this._direccion = direccion;
-      this._ganancias = ganancias;
-    }
-    #endregion
+        #region "Constructores"
+        private Empresa()
+        {
+            this._nominaEmpleados = new List<Empleado>();
+            //alumno
+        }
+        public Empresa(string razonSocial, string direccion, float ganancias)
+        {
+            this._razonSocial = razonSocial;
+            this._direccion = direccion;
+            this._ganancias = ganancias;
+        }
+        #endregion
 
-    #region "Propiedades"
-    //alumno
-    public string RazonSocial
-    {
-      get
-      {
-        return this._razonSocial;
-      }
-      set
-      {
-        this._razonSocial = RazonSocial;
-      }
-    }
-    public string Direccion
-    {
-      get
-      {
-        return this._direccion;
-      }
-      set
-      {
-        this._direccion = Direccion;
-      }
-    }
-    public float Ganancias
-    {
-      get
-      {
-        return this._ganancias;
-      }
-      set
-      {
-        this._ganancias = Ganancias;
-      }
-    }
-    #endregion
+        #region "Propiedades"
+        //alumno
+        public string RazonSocial
+        {
+            get
+            {
+                return this._razonSocial;
+            }
+            set
+            {
+                this._razonSocial = value;
+            }
+        }
+        public string Direccion
+        {
+            get
+            {
+                return this._direccion;
+            }
+            set
+            {
+                this._direccion = value;
+            }
+        }
+        public float Ganancias
+        {
+            get
+            {
+                return this._ganancias;
+            }
+            set
+            {
+                this._ganancias = value;
+            }
+        }
+        #endregion
 
-    #region "Métodos"
-    public string MostrarEmpresa()
-    {
-      StringBuilder sb = new StringBuilder();
-      sb.AppendLine("La empresa " + this.RazonSocial + " sita en la calle " + this.Direccion + " cuenta con ganancias por " + this.Ganancias + " y con " + this._nominaEmpleados.Count + " empleados:");
-      foreach (Empleado e in this._nominaEmpleados)
-      {
-        sb.AppendLine(e.Mostrar());
-      }
-      return sb.ToString();
-    }
-    #endregion
+        #region "Métodos"
+        public string MostrarEmpresa()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("La empresa " + this.RazonSocial + " sita en la calle " + this.Direccion + " cuenta con ganancias por " + this.Ganancias + " y con " + this._nominaEmpleados.Count + " empleados:");
+            foreach (Empleado e in this._nominaEmpleados)
+            {
+                sb.AppendLine(e.Mostrar());
+            }
+            return sb.ToString();
+        }
+        #endregion
 
-    #region "Operadores"
-    /// <summary>
-    /// Agregará un empleado a una empresa, siempre y cuando no figure ya en la nómina
-    /// </summary>
-    /// <param name="empresa">Empresa interesada.</param>
-    /// <param name="empleado">Empleado a agregar.</param>
-    /// <returns></returns>
-    public static Empresa operator +(Empresa empresa, Empleado empleado)
-    {
-      //alumno
-
+        #region "Operadores"
+        /// <summary>
+        /// Agregará un empleado a una empresa, siempre y cuando no figure ya en la nómina
+        /// </summary>
+        /// <param name="empresa">Empresa interesada.</param>
+        /// <param name="empleado">Empleado a agregar.</param>
+        /// <returns></returns>
+        public static Empresa operator +(Empresa empresa, Empleado empleado)
+        {
+            //alumno
+            if (empresa._nominaEmpleados.Contains(empleado))
+            {
+                //El empleado ya se encuentra en la empresa
+            }
+            else
+            {
+                empresa._nominaEmpleados.Add(empleado);
+            }
+            return empresa;
+        }
+        #endregion
     }
-    #endregion
-  }
 }
